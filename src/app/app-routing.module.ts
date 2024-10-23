3732f56ac3b18ff7db04cae479acf618e5c8f021
YuliiaMilian <yuliia@slateteams.com>
2024-10-23 10:41:59 +0300

    Implemented lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,66 +1,56 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppRoutes } from '@Enums/routes.enum';
-import { NotFoundComponent } from './shared/pages/not-found/not-found.component';
-import { SettingGoalsComponent } from './setting-goals/setting-goals.component';
-import { EducationCertificationsComponent } from './education-certifications/education-certifications.component';
-import { ReimbursementOfExpensesComponent } from './reimbursement-of-expenses/reimbursement-of-expenses.component';
-import { FeedbackFormComponent } from './feedback-form/feedback-form.component';
-import { OnboardingPlanComponent } from './onboarding-plan/onboarding-plan.component';
-import { PaidTimeOffComponent } from './paid-time-off/paid-time-off.component';
-import { PerformanceEvaluationComponent } from './performance-evaluation/performance-evaluation.component';
-import { SelfServicePortalComponent } from './self-service-portal/self-service-portal.component';
-import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path: AppRoutes.Empty,
-    redirectTo: AppRoutes.HomePage,
+    loadComponent: () => import('./home/home.component').then((x) => x.HomeComponent),
     pathMatch: 'full',
   },
   {
     path: AppRoutes.HomePage,
-    component: HomeComponent,
+    loadComponent: () => import('./home/home.component').then((x) => x.HomeComponent),
   },
   {
     path: AppRoutes.Education,
-    component: EducationCertificationsComponent,
+    loadComponent: () => import('./education-certifications/education-certifications.component').then((x) => x.EducationCertificationsComponent),
   },
   {
     path: AppRoutes.Expenses,
-    component: ReimbursementOfExpensesComponent,
+    loadComponent: () => import('./reimbursement-of-expenses/reimbursement-of-expenses.component').then((x) => x.ReimbursementOfExpensesComponent),
   },
   {
     path: AppRoutes.FeedbackForm,
-    component: FeedbackFormComponent,
+    loadComponent: () => import('./feedback-form/feedback-form.component').then((x) => x.FeedbackFormComponent),
   },
   {
     path: AppRoutes.Onboarding,
-    component: OnboardingPlanComponent,
+    loadComponent: () => import('./onboarding-plan/onboarding-plan.component').then((x) => x.OnboardingPlanComponent),
   },
   {
     path: AppRoutes.PTO,
-    component: PaidTimeOffComponent,
+    loadComponent: () => import('./paid-time-off/paid-time-off.component').then((x) => x.PaidTimeOffComponent),
   },
   {
     path: AppRoutes.Performance,
-    component: PerformanceEvaluationComponent,
+    loadComponent: () => import('./performance-evaluation/performance-evaluation.component').then((x) => x.PerformanceEvaluationComponent),
   },
   {
     path: AppRoutes.SelfService,
-    component: SelfServicePortalComponent,
+    loadComponent: () => import('./self-service-portal/self-service-portal.component').then((x) => x.SelfServicePortalComponent),
   },
   {
     path: AppRoutes.SettingGoals,
-    component: SettingGoalsComponent,
+    loadComponent: () => import('./setting-goals/setting-goals.component').then((x) => x.SettingGoalsComponent),
   },
   {
     path: AppRoutes.Other,
-    redirectTo: AppRoutes.NotFound,
+    loadComponent: () => import('./shared/pages/not-found/not-found.component').then((x) => x.NotFoundComponent),
   },
   {
     path: AppRoutes.NotFound,
-    component: NotFoundComponent,
+    loadComponent: () => import('./shared/pages/not-found/not-found.component').then((x) => x.NotFoundComponent),
   },
 ];
 
