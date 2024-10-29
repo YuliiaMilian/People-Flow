3d312d668fcbae72b90818c6579709b10879b98f
YuliiaMilian <yuliia@slateteams.com>
2024-10-29 23:33:05 +0200

    Added float label, theme and card

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,16 @@
 import { Component, ContentChild, ElementRef, Input } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { DropdownModule } from 'primeng/dropdown';
 
 @Component({
   selector: 'ngt-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, DropdownModule, CardModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-
 export class HeaderComponent {
   @Input() title: string = '';
   @ContentChild('projectedTitle', { static: false }) projectedTitle!: ElementRef;
@@ -27,5 +30,16 @@ export class HeaderComponent {
     this.projectedParagraph.nativeElement.textContent = 'New paragraph';
   }
 
-}
+  isCardVisible = false;
+  activeCard: string | null = null;
 
+  toggleCard(card: string) {
+    if (this.activeCard === card) {
+      this.isCardVisible = false;
+      this.activeCard = null;
+    } else {
+      this.activeCard = card;
+      this.isCardVisible = true;
+    }
+  }
+}
